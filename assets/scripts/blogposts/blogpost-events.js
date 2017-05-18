d3b428900a37554848eb47a2a2ee4f4f0d8cde84
gastevend <steven.duval@libertymutual.com>
2017-05-18 10:18:46 -0400

    creates a submit button for the edit functionality
    the edit button grabs the header and content of the blog. still need to
    write out the api call but that is next

--- assets/scripts/blogposts/blogpost-events.js
+++ assets/scripts/blogposts/blogpost-events.js
@@ -2,6 +2,7 @@
 
 const api = require('./api')
 const ui = require('./ui')
+const submitEditButtonTemplate = require('../templates/submitEditButtonTemplate.handlebars')
 
 const onDeleteBlog = function (event) {
   event.preventDefault()
@@ -14,12 +15,29 @@ const onDeleteBlog = function (event) {
     .catch(ui.deleteBlogFailure)
 }
 
+const onSubmitEdit = function (event) {
+  event.preventDefault()
+  const blogpostTitle = $(this).parent().find('h2')[0].innerHTML
+  const blogpostContent = $(this).parent().find('p')[0].innerHTML
+  const data = {
+    'blogpostTitle': blogpostTitle,
+    'blogpostContent': blogpostContent
+  }
+  console.log(data)
+  // api.editBlog(data)
+  //   .then(ui.editBlogSuccess)
+  //   .catch(ui.editBlogFailure)
+}
+
 const onEditBlog = function (event) {
   console.log('inside on edit blog')
   const id = $(this).attr('data-id')
   $('p[data-id=' + id + ']').attr('contenteditable', 'true')
   $('h2[data-id=' + id + ']').attr('contenteditable', 'true')
   $('.one-blogpost[data-id=' + id + ']').css('background-color', 'white')
+  const submitEditButtonHtml = submitEditButtonTemplate({})
+  $('#blogpost-container').append(submitEditButtonHtml)
+  $('#submit-edit').on('click', onSubmitEdit)
 }
 
 const onGetBlogpost = function () {
